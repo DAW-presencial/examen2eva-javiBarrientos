aeaf65c7c8de146e8ab8da764b52616459e53a5d
javiBarrientos <javibarrientosfernandez99@gmail.com>
2021-02-24 15:56:09 +0100

    feat: tarea 1, migracion

---
+++ database/migrations/2021_02_24_144446_tutores.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Tutores extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tutores', function (Blueprint $table) {
+            $table->id();
+            $table->string('empresa');
+            $table->string('documento');
+            $table->string('dni');
+            $table->string('nombre');
+            $table->string('primer_apellido');
+            $table->string('segundo_apellido');
+            $table->string('pais');
+            $table->string('provincia');
+            $table->string('municipio');
+            $table->string('estado');
+            $table->integer('telefono');
+            $table->string('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tutores');
+    }
+}
